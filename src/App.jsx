8c285641feeabb0ef0433cf1345e2c5eff96b5d0
noasmith <noasmith@umich.edu>
2024-10-18 12:08:03 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
+function App() { //function name is always capitalized
+  
+
+  return (
+   <main>
+    <TodoInput />
+    <TodoList />
+   </main>
+  )
+}
+
+export default App
