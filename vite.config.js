dd3aff7a3e4c4983d5777625c1aef036f5c2b14a
noa-smith <noasmith@umich.edu>
2024-10-22 13:36:10 -0400

    Fingers crossed Gemini found the issue with vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps:  
+  {  // Add this to fix the fsevents issue
+     exclude: ['fsevents']
+   }
 })
